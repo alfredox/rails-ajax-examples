0d975b6b4f6b334b8dac994f445a96df17f658d7
Alfredo Cruz <alfredox@gmail.com>
2011-07-28 15:55:23 -0500

    Adds actions index, new and create to resource Address

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,2 +1,19 @@
 class AddressesController < ApplicationController
+
+  def index
+    @addresses = Address.all
+  end
+
+  def new
+    @address = Address.new
+  end
+
+  def create
+    @address = Address.new params[:address]
+    if @address.save
+      redirect_to addresses_path
+    else
+      render 'new'
+    end
+  end
 end
