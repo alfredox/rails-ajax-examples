4666093e3f21b85c2ee9bd10bf38fb29f5a0f0f5
Alfredo Cruz <alfredox@gmail.com>
2011-09-26 18:05:28 -0500

    Se creó un plugin de js con la misma funcionalidad

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,43 +1,59 @@
 jQuery(function($) {
-  // when the #address_zip_code field changes
-  $(".zip_code_autocomplete").change(function() {
-    var selectorId = $(this).attr('id'),
-        lastIndex = selectorId.lastIndexOf('zip_code'),
-        selectorPrefix = selectorId.slice(0, lastIndex) ;
 
-    $.get('/addresses/autocomplete_with_zip', 'zip_code=' + $(this).val(), function(data) {
-      $.assignJSONToInputFields(selectorPrefix, data) ;
-    });
-  });
+  // plugin: name ajaxDataCall //temporal
+  (function( $ ){
+    $.fn.ajaxDataCall = function(url, fieldName) {
 
-  // using the data in json, it assigns the value to
-  $.assignJSONToInputFields = function (selectorPrefix, json) {
-    for (var key in json) {
-      var $inputTag = $('#' + selectorPrefix + key) ;
-      if ($inputTag.prop('tagName') == 'SELECT') {
-        $inputTag.html($.createHTMLOptionsStringFromCollection(json[key])) ;
-      }
-      else {
-        // normal assignation
-        $inputTag.val(json[key]) ;
-      }
-    }
-  } ;
+      var createHTMLOptionsStringFromCollection = function (collection) {
+        if (collection instanceof Array) {
+          var optionString = '' ;
+          for (var elementIndex in collection) {
+            var element = collection[elementIndex] ;
+            if (element instanceof Array)
+              optionString = optionString + '<option value="' + element[1] +'">' + element[0] + '</option>'
+            else
+              optionString = optionString + '<option value="' + element +'">' + element + '</option>' ;
+          }
+          return optionString ;
+        }
+        else {
+          return "<option>" + collection + "</option>" ;
+        }
+      } ;
 
-  $.createHTMLOptionsStringFromCollection = function (collection) {
-    if (collection instanceof Array) {
-      var optionString = '' ;
-      for (var elementIndex in collection) {
-        var element = collection[elementIndex] ;
-        if (element instanceof Array)
-          optionString = optionString + '<option value="' + element[1] +'">' + element[0] + '</option>'
-        else
-          optionString = optionString + '<option value="' + element +'">' + element + '</option>' ;
-      }
-      return optionString ;
-    }
-    else {
-      return "<option>" + collection + "</option>" ;
-    }
-  } ;
+      // using the data in json, it assigns the value to each selector
+      var assignJSONToInputFields = function (selectorPrefix, json) {
+        for (var key in json) {
+          var $inputTag = $('#' + selectorPrefix + key) ;
+          if ($inputTag.prop('tagName') == 'SELECT') {
+            $inputTag.html(createHTMLOptionsStringFromCollection(json[key])) ;
+          }
+          else {
+            // normal assignation
+            $inputTag.val(json[key]) ;
+          }
+        }
+      } ;
+
+      // main function
+      return this.each(function() {
+
+        // acts with the change event
+        $(this).change(function() {
+          // finds the prefix of the selector, so it can be used to find the other selectors
+          // to update
+          var selectorId = $(this).attr('id'),
+              lastIndex = selectorId.lastIndexOf(fieldName),
+              selectorPrefix = selectorId.slice(0, lastIndex) ;
+
+          $.get(url, fieldName + '=' + $(this).val(), function(data) {
+            assignJSONToInputFields(selectorPrefix, data) ;
+          });
+        });
+      }) ;
+    };
+  })( jQuery );
+
+  $(".zip_code_autocomplete").ajaxDataCall('/addresses/autocomplete_with_zip', 'zip_code');
+
 })
